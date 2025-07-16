4c06f30ceac06e6362906d11ed4bbe962af87d10
박찬혁 <cksgurrhtn@naver.com>
2025-07-16 14:09:50 +0900

    commit

---
+++ src/main/java/com/chan7731/event_reservation/service/ReservationService.java
@@ -0,0 +1,32 @@
+package com.chan7731.event_reservation.service;
+
+
+import com.chan7731.event_reservation.dto.ReservationDto;
+import com.chan7731.event_reservation.model.Event;
+import com.chan7731.event_reservation.model.Reservation;
+import com.chan7731.event_reservation.repository.EventRepository;
+import com.chan7731.event_reservation.repository.ReservationRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ReservationService {
+    private final ReservationRepository reservationRepository;
+    private final EventRepository eventRepository;
+
+    public Reservation create(Long eventId, ReservationDto reservationDto) {
+        Event event = eventRepository.findById(eventId).orElseThrow(() -> new NoSuchElementException("이벤트 없음"));
+
+        Reservation reservation = new Reservation();
+        reservation.setEvent(event);
+        reservation.setAttendeeName(reservationDto.getAttendeeName());
+        reservation.setSeat(reservationDto.getSeats());
+
+        return reservationRepository.save(reservation);
+    }
+}
